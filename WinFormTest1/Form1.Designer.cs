2ef69cb7aacffa4db36423c28cb8a79637d0ac42
SAUD ALOTAIBI <s44451484@students.tu.edu.sa>
2024-05-09 15:22:15 +0300

    ABDULRAHMAN MOHAMMED SINDI 44459805 683

--- WinFormTest1/Form1.Designer.cs
+++ WinFormTest1/Form1.Designer.cs
@@ -38,61 +38,70 @@
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(56, 49);
+            textBox1.Location = new Point(80, 82);
+            textBox1.Margin = new Padding(4, 5, 4, 5);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(100, 23);
+            textBox1.Size = new Size(141, 31);
             textBox1.TabIndex = 0;
             // 
             // textBox2
             // 
-            textBox2.Location = new Point(272, 49);
+            textBox2.Location = new Point(389, 82);
+            textBox2.Margin = new Padding(4, 5, 4, 5);
             textBox2.Name = "textBox2";
-            textBox2.Size = new Size(100, 23);
+            textBox2.Size = new Size(141, 31);
             textBox2.TabIndex = 0;
             // 
             // textBox3
             // 
-            textBox3.Location = new Point(56, 195);
+            textBox3.Location = new Point(80, 325);
+            textBox3.Margin = new Padding(4, 5, 4, 5);
             textBox3.Name = "textBox3";
-            textBox3.Size = new Size(100, 23);
+            textBox3.Size = new Size(141, 31);
             textBox3.TabIndex = 0;
             // 
             // textBox4
             // 
-            textBox4.Location = new Point(272, 195);
+            textBox4.Location = new Point(389, 325);
+            textBox4.Margin = new Padding(4, 5, 4, 5);
             textBox4.Name = "textBox4";
-            textBox4.Size = new Size(100, 23);
+            textBox4.Size = new Size(141, 31);
             textBox4.TabIndex = 0;
             // 
             // button1
             // 
-            button1.Location = new Point(126, 98);
+            button1.Location = new Point(180, 163);
+            button1.Margin = new Padding(4, 5, 4, 5);
             button1.Name = "button1";
-            button1.Size = new Size(154, 23);
+            button1.Size = new Size(220, 38);
             button1.TabIndex = 1;
             button1.Text = "دور في اتجاه عقارب الساعة";
             button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
             // 
             // button2
             // 
-            button2.Location = new Point(126, 139);
+            button2.Location = new Point(180, 232);
+            button2.Margin = new Padding(4, 5, 4, 5);
             button2.Name = "button2";
-            button2.Size = new Size(154, 23);
+            button2.Size = new Size(220, 38);
             button2.TabIndex = 1;
             button2.Text = "دور عكس اتجاه عقارب الساعة";
             button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(417, 289);
+            ClientSize = new Size(596, 482);
             Controls.Add(button2);
             Controls.Add(button1);
             Controls.Add(textBox4);
             Controls.Add(textBox3);
             Controls.Add(textBox2);
             Controls.Add(textBox1);
+            Margin = new Padding(4, 5, 4, 5);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
